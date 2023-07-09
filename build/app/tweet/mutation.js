d92a983023040d95be10ad91d20a4b11ff99ac46
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-07-09 21:05:05 +0530

    remove

--- build/app/tweet/mutation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.muatations = void 0;
-exports.muatations = `#graphql
-    createTweet(payload: CreateTweetData!): Tweet
-`;
--- build/app/user/mutations.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mutations = void 0;
-exports.mutations = `#graphql
-    followUser(to: ID!): Boolean
-    unfollowUser(to: ID!): Boolean
-`;
